0c484e6accb32e7d66a210c6faff9b51476984f8
maks <maks-lapin-1998@.ru>
2023-05-06 20:37:29 +0400

    task41

---
+++ task41/Program.cs
@@ -0,0 +1,35 @@
+﻿int ReadInt(string argument) // проверка на число
+{
+    Console.Write(argument);
+    int i;
+    while (!int.TryParse(Console.ReadLine(), out i))
+    {
+        System.Console.WriteLine("Это не число!");
+        Console.Write(argument);
+    }
+    return i;
+}
+int[] MakeArray(int m) //массив длинной m
+{
+    int[] array = new int [m];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = ReadInt("Введите число: "); 
+    }
+    return array;
+}
+int NotZerro(int[]array) //проверяем сколько чисел больше нуля
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int m = ReadInt("Укажите количество чисел: ");
+System.Console.WriteLine($"{NotZerro(MakeArray(m))} чисел(а) больше нуля");
